6adb6a92c39bbcceb7dacc012146684d68429e4a
carlosfloresta <47724025+carlosfloresta@users.noreply.github.com>
2019-03-22 01:08:12 -0300

    Add files via upload

---
+++ src/com/agenda/dao/PessoaDAO.java
@@ -0,0 +1,14 @@
+package com.agenda.dao;
+
+import com.agenda.model.Pessoa;
+
+public class PessoaDAO {
+	
+	public void conexao(Pessoa pessoa) {
+		
+		
+		System.out.println(pessoa.getNome());
+		
+	}
+
+}
---
+++ src/com/agenda/service/CadastraUsuarioService.java
@@ -0,0 +1,18 @@
+package com.agenda.service;
+
+import com.agenda.dao.PessoaDAO;
+import com.agenda.model.Pessoa;
+
+public class CadastraUsuarioService {
+	
+	public void cadastra(Pessoa pessoa) {
+		
+	
+		PessoaDAO dao = new PessoaDAO();
+		
+		dao.conexao(pessoa);
+		
+	}
+	
+
+}
